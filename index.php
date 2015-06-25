0d95e654b9a68f457d7d3d6efca6239796b1fc6e
CristinaPerea <criperrui@gmail.com>
2015-06-25 18:08:50 +0200

    cambios publicaciones
    cambios

--- index.php
+++ index.php
@@ -288,120 +288,21 @@
             </section>
             <!-- End partners -->   
               
-          <!-- Begin cta -->
-          <section id="cta-section">
-          	<div class="cta">
-            	<div class="container">
-                	<div class="row">
-                    
-                    	<!-- Begin page header-->
-                        <div class="page-header-wrapper">
-                            <div class="container">
-                                <div class="page-header text-center wow fadeInUp" data-wow-delay="0.3s">
-                                    <h2>Publicaciones</h2>
-                                    <div class="devider"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End page header-->
-                        
-                    </div> <!-- /.row -->
-
+            <section id="cta-section" class="page text-center">
+                <!-- Begin page header-->
+                <div class="page-header-wrapper">
                     <div class="container">
-                    <body>
-
-                        <form action="" id="quicksearch">
-                        <input type="text" id="qs_field" autocomplete="off" placeholder="Type to search..." /> <input type="button" onclick="clearQS()" value="clear" />
-                        <span id="searchstat">Matching entries: <span id="stat">0</span></span>
-                        <div id="showsettings" onclick="toggleSettings()">settings...</div>
-                        <div id="settings" class="hidden">
-                        <ul>
-                        <li><input type="checkbox" class="search_setting" id="opt_searchAbs" onchange="updateSetting(this)"><label for="opt_searchAbs"> include abstract</label></li>
-                        <li><input type="checkbox" class="search_setting" id="opt_searchRev" onchange="updateSetting(this)"><label for="opt_searchRev"> include review</label></li>
-                        <li><input type="checkbox" class="search_setting" id="opt_useRegExp" onchange="updateSetting(this)"><label for="opt_useRegExp"> use RegExp</label></li>
-                        <li><input type="checkbox" class="search_setting" id="opt_noAccents" onchange="updateSetting(this)"><label for="opt_noAccents"> ignore accents</label></li>
-                        </ul>
+                        <div class="page-header text-center wow fadeInDown" data-wow-delay="0.4s">
+                            <h2>Publicaciones</h2>
+                                    <div class="devider"></div>
+                                     <div class="responsive-iframe-container">
+                                  <iframe src="test-table-pro.html"></iframe>
+                                    </div>
                         </div>
-                        </form>
-                        <table id="qs_table" border="1">
-                        <thead><tr><th width="20%">Author</th><th width="30%">Title</th><th width="5%">Year</th><th width="30%">Journal/Proceedings</th><th width="10%">Reftype</th><th width="5%">DOI/URL</th></tr></thead>
-                        <tbody><tr id="" class="entry">
-                            <td>Cabanillas, C., Resinas, M., Mendling, J. and Ruiz-Cort&eacute;s, A.</td>
-                            <td>Automated Team Selection and Compliance Checking in Business Processes <p class="infolinks">[<a href="javascript:toggleInfo('','bibtex')">BibTeX</a>]</p></td>
-                            <td>In Press</td>
-                            <td>International Conference on Software and Systems Processes (ICSSP)&nbsp;</td>
-                            <td>conference</td>
-                            <td>&nbsp;</td>
-                        </tr>
-                        <tr id="bib_" class="bibtex noshow">
-                        <td colspan="6"><b>BibTeX</b>:
-                        <pre>
-                        @conference{,
-                          author = {Cristina Cabanillas and Manuel Resinas and Jan Mendling and Antonio Ruiz-Cort&eacute;s},
-                          title = {Automated Team Selection and Compliance Checking in Business Processes},
-                          booktitle = {International Conference on Software and Systems Processes (ICSSP)},
-                          year = {In Press}
-                        }
-                        </pre></td>
-                        </tr>
-                        <tr id="" class="entry">
-                            <td>Garcia-Gal&aacute;n, J., Trinidad, P., Rana, O.F. and Ruiz-Cort&eacute;s, A.</td>
-                            <td>Automated Configuration Support for Infrastructure Migration to the Cloud <p class="infolinks">[<a href="javascript:toggleInfo('','bibtex')">BibTeX</a>]</p></td>
-                            <td>In Press</td>
-                            <td>Future Generation Computer Systems&nbsp;</td>
-                            <td>article</td>
-                            <td><a href="http://dx.doi.org/10.1016/j.future.2015.03.006">DOI</a> &nbsp;</td>
-                        </tr>
-                        <tr id="bib_" class="bibtex noshow">
-                        <td colspan="6"><b>BibTeX</b>:
-                        <pre>
-                        @article{,
-                          author = {Jes&uacute;s Garcia-Gal&aacute;n and Pablo Trinidad and Rana, Omer F. and Antonio Ruiz-Cort&eacute;s},
-                          title = {Automated Configuration Support for Infrastructure Migration to the Cloud},
-                          journal = {Future Generation Computer Systems},
-                          year = {In Press},
-                          doi = {http://dx.doi.org/10.1016/j.future.2015.03.006}
-                        }
-                        </pre></td>
-                        </tr>
-                        </tbody>
-                        </table>
-                        <!-- file generated by JabRef -->
-                        </body>
-                       <!-- <div class="col-md-12">
-                            <ul class="">
-                                <li class="col-md-6">
-                                    <h4 class="col-md-1"><span class="icon-book"></span></h4>
-                                    <div class="rotate-box-info">
-                                        <h4>Publicación 1</h4>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                                    </div>
-                                    </a>
-                                </li>
-                                <li class="col-md-6">
-                                    <h4 class="col-md-1"><span class="icon-book"></span></h4>
-                                    <div class="rotate-box-info">
-                                        <h4>Publicación 2</h4>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                                    </div>
-                                    </a>
-                                </li>
-                                <li class="col-md-6">
-                                    <h4 class="col-md-1"><span class="icon-book"></span></h4>
-                                    <div class="rotate-box-info">
-                                        <h4>Publicación 3</h4>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                                    </div>
-                                    </a>
-                                </li>
-                              </ul>
-                        </div>-->
                     </div>
-    
-                </div> 
-            </div>
-          </section>
-          <!-- End cta -->
+                </div>
+              </section>
+
          <section id="testimonial-section">
                 <div id="testimonial-trigger" class="testimonial text-white parallax" data-stellar-background-ratio="0.5" style="background-image: url(img/testimonial-bg.jpg);">
                     <div class="cover"></div>
