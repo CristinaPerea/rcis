ca4b96f64e11554349b16036c620e804546ab69a
CristinaPerea <criperrui@gmail.com>
2015-07-06 11:01:44 +0200

    Tarea #813
    Todavía faltan modificaciones en el csv de herramientas

--- sliderHerramientasJSON.php
+++ sliderHerramientasJSON.php
@@ -3,14 +3,17 @@
 
 	$arrayHerramientas = array(); 
 	$arrayHerramientas = loadDataFromCSV("https://docs.google.com/spreadsheets/d/1r_HzzjbG93PYkgZXTz7vwwxR7pCIilCSLb-m3t_rzic/export?format=csv&id=1r_HzzjbG93PYkgZXTz7vwwxR7pCIilCSLb-m3t_rzic");
+	$arrayGrupos = loadDataFromCSV("https://docs.google.com/spreadsheets/d/1wpxEusyVf_MfIX595aI0BIKZnNiW269xii3bLAhNscY/export?format=csv");
 	//print_r($arrayHerramientas);
 
 	$nombreHerramienta;
-	$enlaceLogoGrupo;
+	$enlaceLogoGrupo = "";
 	$enlaceLogoHerramienta;
 	$imagenDeHerramienta;
 	$descripcion;
 	$enlaceWeb;
+	$siglasGrupo;
+	$enlaceLogoUniversidad = "";
 
 	/* Randomizar el slider */
 	$numeroTotalDeHerramientas = sizeof($arrayHerramientas)-1;
@@ -32,30 +35,40 @@
 		$numeroDeHerramientasAMostrarEnSlider = $numeroTotalDeHerramientas;
 	
 	for ($i = 0; $i < $numeroDeHerramientasAMostrarEnSlider; $i++) { 
-			$nombreHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][1];
-			$enlaceLogoGrupo = $arrayHerramientas[$arrayDeIndices[$i]][2];
-			$enlaceLogoHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][10];
-			$imagenDeHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][4];
-			$descripcion = $arrayHerramientas[$arrayDeIndices[$i]][5];
-			$enlaceWeb = $arrayHerramientas[$arrayDeIndices[$i]][6];
-			//echo $nombreHerramienta.$enlaceLogoHerramienta.$enlaceLogoGrupo.$imagenDeHerramienta.$descripcion.$enlaceWeb;
+		$nombreHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][1];
+		//$enlaceLogoGrupo = $arrayHerramientas[$arrayDeIndices[$i]][2];
+		$enlaceLogoHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][10];
+		$imagenDeHerramienta = $arrayHerramientas[$arrayDeIndices[$i]][4];
+		$descripcion = $arrayHerramientas[$arrayDeIndices[$i]][5];
+		$enlaceWeb = $arrayHerramientas[$arrayDeIndices[$i]][6];
+		$siglasGrupo = $arrayHerramientas[$arrayDeIndices[$i]][8];
 
-					$elemento = '<div class="container item"><div class="slider-fila-1"><div class="slider-logo-1 col-md-6 col-xs-6"><img src="';
-					$elemento .= $enlaceLogoHerramienta;
-					$elemento .= '" class="img-responsive logos-de-slider"/></div><div class="slider-logo-2 col-md-6 col-xs-6 "><img src="';
-					$elemento .= $enlaceLogoGrupo;
-					$elemento .= '" class="img-responsive logos-de-slider logo-derecha-slider"/></div></div>';
-					$elemento .= '<div class="slider-fila-2"><div class="col-md-6"><img src="';
-					$elemento .= $imagenDeHerramienta;
-					$elemento .= '"class="img-responsive img-responsive-capturas"/></div><div class="col-md-6"><p 	class="texto-slider-herramientas">';
-					$temp = $nombreHerramienta.'</p>';
-					$elemento .= $temp;
-					$elemento .= '<p class="texto-slider-herramientas-descripcion">';
-					$elemento .= $descripcion.'</p>'	;
-					$elemento .= '<br><br><br><a href="';
-					$elemento .= $enlaceWeb;
-					$elemento .= '" class="btn btn-default">Más información</a></div></div></div>';
-					echo $elemento;
+		for($iteGrupo = 1; $iteGrupo < sizeof($arrayGrupos); $iteGrupo++){
+			if($siglasGrupo == $arrayGrupos[$iteGrupo][0]){
+				$enlaceLogoGrupo = $arrayGrupos[$iteGrupo][5];
+				$enlaceLogoUniversidad = $arrayGrupos[$iteGrupo][8];
+			}
+		}
+
+		if($enlaceLogoGrupo == "")
+			$enlaceLogoGrupo = $enlaceLogoUniversidad;
+
+		$elemento = '<div class="container item"><div class="slider-fila-1"><div class="slider-logo-1 col-md-6 col-xs-6"><img src="';
+		$elemento .= $enlaceLogoHerramienta;
+		$elemento .= '" class="img-responsive logos-de-slider"/></div><div class="slider-logo-2 col-md-6 col-xs-6 "><img src="';
+		$elemento .= $enlaceLogoGrupo;
+		$elemento .= '" class="img-responsive logos-de-slider logo-derecha-slider"/></div></div>';
+		$elemento .= '<div class="slider-fila-2"><div class="col-md-6"><img src="';
+		$elemento .= $imagenDeHerramienta;
+		$elemento .= '"class="img-responsive img-responsive-capturas"/></div><div class="col-md-6"><p 	class="texto-slider-herramientas">';
+		$temp = $nombreHerramienta.'</p>';
+		$elemento .= $temp;
+		$elemento .= '<p class="texto-slider-herramientas-descripcion">';
+		$elemento .= $descripcion.'</p>'	;
+		$elemento .= '<br><br><br><a href="';
+		$elemento .= $enlaceWeb;
+		$elemento .= '" class="btn btn-default">Más información</a></div></div></div>';
+		echo $elemento;
 
 	}
 
